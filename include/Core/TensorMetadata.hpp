1634845a8302006a2a9be48ad91483bc5f567e32
Eren64bit <ozcomlekciahmeteren@gmail.com>
2025-09-06 22:04:37 +0300

    Added Rank var and GetRank Function

--- include/Core/TensorMetadata.hpp
+++ include/Core/TensorMetadata.hpp
@@ -15,8 +15,10 @@ namespace NextMetadata
         TensorStrideDynamic strides_; // Strides of the tensor (e.g., {12, 4, 1} for a 2x3x4 tensor) TensorStrideDynamic = std::vector<size_t>
         TensorOffset offset_;         // Offset in the underlying data array
         TensorSize totalSize_;        // Total number of elements in the tensor
+        TensorRank rank_;             // Rank (number of dimensions) of the tensor
         bool isContiguous_;           // Whether the tensor is stored in contiguous memory
     public:
+    
         // Constructor
         /** 
          * @brief Constructs a TensorMetadata object with the given shape and optional offset.
@@ -28,9 +30,10 @@ namespace NextMetadata
             : shape_(shape), offset_(offset){
                 strides_ = NextUtils::ComputeStrides(shape_);
                 totalSize_ = NextUtils::ComputeSize(shape_);
+                rank_ = shape_.size();
                 isContiguous_ = true; // Assuming newly created tensors are contiguous
         }
-        
+
         /** 
          * @brief Constructs a TensorMetadata object with the given shape, strides, and optional offset.
          * @param shape The shape of the tensor.
@@ -41,40 +44,54 @@ namespace NextMetadata
         TensorMetadata(const TensorShapeDynamic &shape, TensorStrideDynamic &stride, TensorOffset offset = 0) noexcept
             : shape_(shape), strides_(stride), offset_(offset){
                 totalSize_ = NextUtils::ComputeSize(shape_);
+                rank_ = shape_.size();
                 isContiguous_ = true; // Assuming newly created tensors are contiguous
         }
+
         /** 
          * @brief Gets the shape of the tensor.
          * @param shape The new shape of the tensor.
          * @return A reference to the shape of the Metadata object.
          * **/
         [[nodiscard]] const TensorShapeDynamic& GetShape() const noexcept { return shape_; }
+
         /** 
          * @brief Gets the strides of the tensor.
          * @param shape The new shape of the tensor.
          * @return A reference to the strides of the Metada object.
          * **/  
         [[nodiscard]] const TensorStrideDynamic& GetStrides() const noexcept { return strides_; }
+
         /** 
          * @brief Gets the offset of the tensor.
          * @return The offset of the tensor.
          * **/
         [[nodiscard]] TensorOffset GetOffset() const noexcept { return offset_; }
+
         /** 
          * @brief Gets the total size of the tensor.
          * @return The total number of elements in the tensor.
          * **/
         [[nodiscard]] TensorSize GetTotalSize() const noexcept { return totalSize_; }
+
+        /** 
+         * @brief Gets the rank of the tensor.
+         * @return The rank (number of dimensions) of the tensor.
+         * **/
+        [[nodiscard]] TensorRank GetRank() const noexcept { return rank_; }
+
         /** 
          * @brief Checks if the tensor is contiguous in memory.
          * @return True if the tensor is contiguous, false otherwise.
          * **/
         [[nodiscard]] bool IsContiguous() const noexcept { return isContiguous_; }
+
         /** 
          * @brief Sets the contiguity of the tensor.
          * @param contiguous True if the tensor is contiguous, false otherwise.
          * **/
         void SetContiguous(bool contiguous) noexcept { isContiguous_ = contiguous; }
+
     };
 
 }
