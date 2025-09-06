0122288a9885f2bae23f3e4d85019ca032f9075e
Eren64bit <ozcomlekciahmeteren@gmail.com>
2025-09-06 22:31:01 +0300

    Added Tensor Interface Consstructers

--- include/Core/TensorInterface.hpp
+++ include/Core/TensorInterface.hpp
@@ -12,7 +12,26 @@ namespace NextTensor
      * Members : metadata_, dtype_
      * **/
     class TensorInterface {
+    public:
+        /** 
+         * @brief Constructs a TensorInterface object with the given shape, data type, and optional offset.
+         * @param shape The shape of the tensor.
+         * @param dtype The data type of the tensor elements.
+         * @param offset The offset in the underlying data array (default is 0).
+         * @return A TensorInterface object initialized with the given shape, data type, and offset.
+         * **/
+        TensorInterface(const TensorShapeDynamic &shape, DataType dtype, TensorOffset offset = 0) noexcept
+            : metadata_(shape, offset) , dtype_(dtype) {}
+            
+        /** 
+         * @brief Constructs a TensorInterface object with the given metadata and data type.
+         * @param metadata The metadata of the tensor.
+         * @param dtype The data type of the tensor elements.
+         * @return A TensorInterface object initialized with the given metadata and data type.
+         * **/
+        TensorInterface(const TensorMetadata &metadata, DataType dtype) noexcept : metadata_(metadata), dtype_(dtype){}
 
+        virtual ~TensorInterface() = default;
     protected:
         TensorMetadata metadata_; // Metadata of the tensor (shape, strides, offset, etc.)
         DataType dtype_;          // Data type of the tensor elements (e.g., float32, int64, etc.)
